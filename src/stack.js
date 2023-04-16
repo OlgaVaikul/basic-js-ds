ca9051b010d2b7d476f67f63ff11e8b26677771d
Olga Vaikul <olenka1201@list.ru>
2023-04-16 08:24:23 +0300

    complete task

--- src/stack.js
+++ src/stack.js
@@ -13,6 +13,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
+      constructor()
+    {
+        this.stack = [];
+    }
   push(element) {
    this.stack.push(element);
   }
@@ -22,8 +26,7 @@ class Stack {
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  return this.stack[this.stack.length - 1];
   }
 }
 
